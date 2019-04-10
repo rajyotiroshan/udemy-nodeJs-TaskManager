72f1a53abd9e253463506525104b4ecb2e2c45c0
rajan <rajangreedge@gmail.com>
2019-04-10 18:44:11 +0530

    fetch data for user and task

--- src/index.js
+++ src/index.js
@@ -19,6 +19,29 @@ app.post('/users', (req, res)=> {
     });
 })
 
+//hand;er for user data request
+app.get('/users', (req, res)=>{
+    User.find({}).then((users)=>{
+        res.send(users);
+    }).catch((error)=>{
+        res.status(500).send();
+    });
+});
+
+app.get('/users/?:id', (req,res)=>{
+    //req.params object has single property as id.
+    const _id = req.params.id;//access id from url.
+    User.findById(_id).then((user)=>{
+        if(!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((e)=>{
+        res.status(500).send();
+    });
+})
+
 //listen for task creation req.
 app.post('/tasks', (req, res)=> {
     //create new task from upcoming data.
@@ -32,6 +55,28 @@ app.post('/tasks', (req, res)=> {
 
 });
 
+//fetch task.
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks);
+    }).catch((e)=>{
+        res.send(e);
+    })
+})
+
+//fetch a task of provided id.
+
+app.get('/tasks/?:id',(req, res)=>{
+    const _id = req.params.id;
+    Task.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((e)=>{
+        res.status(500).send();
+    });
+})
 
 app.listen(port, ()=> {
     console.log('Server is up on port' + port);
