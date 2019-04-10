415dc236a0163cab2db1d27a512751f3df01df6e
rajan <rajangreedge@gmail.com>
2019-04-10 17:40:15 +0530

    only db connnect for mngoose file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,41 +3,7 @@ const mongoose = require('mongoose');
 //connect to the db by passing URL(mongodb://127.0.0.1:27017) and databse name(task-manager-api)
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
-    useCreateIndex: true
-    
+    useCreateIndex: true   
 })
 
-//Challenges.
-
-// 1. Create a model for task.
-
-/* const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-
-    },
-    completed: {
-        type: Boolean,
-        completed: true
-    }
-});
-
-//2. create an instance.
-const task = new Task({
-    description: 'Eat Lunch'
-});
-
-//3. save into the databse.
-
-task.save().then((insertedTask)=>{
-    console.log(insertedTask);
-}).catch((error)=>{
-    console.log("Error::", error);
-});  */
-
-/**
- * Data Validation And Data sanitization.
- */
 
