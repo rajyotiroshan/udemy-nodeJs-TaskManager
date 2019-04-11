c41350a524bc93d721cbe66405b83390c9569eef
rajan <rajangreedge@gmail.com>
2019-04-11 18:21:52 +0530

    used async-await instead of promise then to communicate with db

--- src/index.js
+++ src/index.js
@@ -9,29 +9,53 @@ const port = process.env.PORT || 3000;
 app.use(express.json());//parse incoming json to an object 
 
 //listen for user creation request.
-app.post('/users', (req, res)=> {
+app.post('/users', async (req, res)=> {
     const user = new User(req.body);
-    user.save().then(()=>{
-        res.send(user);
+    /*     user.save().then(()=>{
+        res.status(201).send(user);
     }).catch((error)=>{
         res.status(400);
         res.send(error);
-    });
+    }); */
+    try {
+      await user.save();
+      res.status(201).send(user);  
+    }catch(e){
+        res.status(400).send(e);
+    }
+    
 })
 
 //hand;er for user data request
-app.get('/users', (req, res)=>{
-    User.find({}).then((users)=>{
+app.get('/users', async (req, res)=>{
+
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }catch(e) {
+        res.status(500).send(e);
+    }
+
+/*     User.find({}).then((users)=>{
         res.send(users);
     }).catch((error)=>{
         res.status(500).send();
-    });
+    }); */
 });
 
-app.get('/users/?:id', (req,res)=>{
+app.get('/users/?:id', async (req,res)=>{
     //req.params object has single property as id.
     const _id = req.params.id;//access id from url.
-    User.findById(_id).then((user)=>{
+    try {
+        const user = User.findById(_id);
+        if(!user){
+            return res.status(404).send();
+        }
+        res.send(user);
+    }catch(e){
+        res.status(500).send();
+    }
+/*     User.findById(_id).then((user)=>{
         if(!user) {
             return res.status(404).send();
         }
@@ -39,43 +63,69 @@ app.get('/users/?:id', (req,res)=>{
         res.send(user);
     }).catch((e)=>{
         res.status(500).send();
-    });
+    }); */
 })
 
 //listen for task creation req.
-app.post('/tasks', (req, res)=> {
+app.post('/tasks', async  (req, res)=> {
     //create new task from upcoming data.
+
     const task = new Task(req.body);
+    try {
+       await task.save(); 
+       res.status(201).send(task);
+    }catch(e){
+        res.status(400).send(error);
+    }
+    
+/*     const task = new Task(req.body);
     //save task to db.
     task.save().then(()=>{
         res.status(201).send(task);
     }).catch((error)=>{
         res.status(400).send(error);
-    })
+    }) */
 
 });
 
 //fetch task.
-app.get('/tasks',(req,res)=>{
-    Task.find({}).then((tasks)=>{
+app.get('/tasks',async (req,res)=>{
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks)
+    }catch(e){
+        res.send(e);
+    }
+
+/*     Task.find({}).then((tasks)=>{
         res.send(tasks);
     }).catch((e)=>{
         res.send(e);
-    })
+    }) */
 })
 
 //fetch a task of provided id.
 
-app.get('/tasks/?:id',(req, res)=>{
+app.get('/tasks/?:id', async (req, res)=>{
     const _id = req.params.id;
-    Task.findById(_id).then((task)=>{
+    try {
+        const task = await Task.findById(_id);
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    }catch(e){
+        res.status(500).send(e);
+    }
+
+/*     Task.findById(_id).then((task)=>{
         if(!task){
             return res.status(404).send();
         }
         res.send(task);
     }).catch((e)=>{
         res.status(500).send();
-    });
+    }); */
 })
 
 app.listen(port, ()=> {
