d33afbbc3060e03a9a5cfe932926f7f0879aabcf
rajan <rajangreedge@gmail.com>
2019-04-11 18:53:13 +0530

    update user with id

--- src/index.js
+++ src/index.js
@@ -128,6 +128,28 @@ app.get('/tasks/?:id', async (req, res)=>{
     }); */
 })
 
+//patch: to update an existing request.
+
+app.patch('/users/:id', async (req, res)=>{
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidUpdateRequest = updates.every(update=> allowedUpdates.includes(update));
+
+    if(isValidUpdateRequest){
+        return res.status(404).send('error: Invalid field reqest for update')
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true});
+        if(!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
 app.listen(port, ()=> {
     console.log('Server is up on port' + port);
 })
