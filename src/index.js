0535099ccf1eb68a3eca6a515725049d9528d490
rajan <rajangreedge@gmail.com>
2019-04-11 19:25:28 +0530

    update task , delete user of given id

--- src/index.js
+++ src/index.js
@@ -66,6 +66,20 @@ app.get('/users/?:id', async (req,res)=>{
     }); */
 })
 
+//delete a user of given id
+
+app.delete('/users/:id', async (req, res)=>{
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user) {
+            return res.status(404).send();
+        }
+        res.send(user)
+    }catch(e){
+        res.status(500).send();
+    }
+})
+
 //listen for task creation req.
 app.post('/tasks', async  (req, res)=> {
     //create new task from upcoming data.
@@ -128,7 +142,7 @@ app.get('/tasks/?:id', async (req, res)=>{
     }); */
 })
 
-//patch: to update an existing request.
+//patch: to update an existing request. (for user).
 
 app.patch('/users/:id', async (req, res)=>{
     const updates = Object.keys(req.body);
@@ -149,6 +163,29 @@ app.patch('/users/:id', async (req, res)=>{
         res.status(400).send(e);
     }
 })
+
+//update an existing task field.
+
+app.patch('/tasks/:id', async (req, res)=>{
+    const allowedUpdates = ['description', 'completed'];
+    const reqUpdates = Object.keys(req.body);
+    const isValidReqForUpdate = reqUpdates.every(update => allowedUpdates.includes(update));
+    
+    if(!isValidReqForUpdate){
+        return res.status(400).send('error: invalid field request for update');
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if(!task) {
+            res.status(404).send();
+        }
+        res.send(task);
+    }catch(e){
+        res.status(400).send(e);
+    }
+
+});
 
 app.listen(port, ()=> {
     console.log('Server is up on port' + port);
