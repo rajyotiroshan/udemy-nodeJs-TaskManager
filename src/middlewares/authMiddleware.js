021ac6ccb448ba8975773587b56a3a095b3c86a8
rajan <rajangreedge@gmail.com>
2019-04-22 20:50:15 +0530

    authmiddleware as middleware for route

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
 const authMiddleware = async (req,res,next)=>{
-    console.log('auth middleware');
-    next();
+    try {
+        console.log(req.header('Authorization'));
+        const token = req.header('Authorization').replace('Bearer ', '');
+        //console.log(token);
+        const decoded = jwt.verify(token, 'thisismynewcourse');
+        const user = await User.findOne({_id: decoded._id, 'tokens.token':token});
+
+        if(!user) {
+            throw new Error("");
+        }
+
+        req.token = token;
+        //store user to req will be access by route handler.
+        req.user = user;
+
+        next();
+    }catch(e){
+        res.status(401).send("error: Invalid credentials.");
+    }
 }
 
 module.exports = authMiddleware;
