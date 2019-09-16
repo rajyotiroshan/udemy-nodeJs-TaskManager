52d8e3ffebfd0f4dea6cff383fa48cffae7eb3e2
rajan <rajangreedge@gmail.com>
2019-09-16 15:10:57 +0530

    commented test function

--- src/index.js
+++ src/index.js
@@ -6,26 +6,8 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//middlewares.
-/* app.use((req,res,next)=>{
-    //console.log(req.method,req.path);
-    //call next in chain.
-    if(req.method === 'GET'){
-        res.send('GET ')
-    }else {
-        next();//handle next route handler.//done with middleware.
-    }
-   
-}); */
-
-//Challenge
-/***middleware for maintenance mode. */
-/* app.use((req,res,next)=>{
-    res.status(503).send('Site is under maintenance.')
-}); */
-
 // router.
-app.use(express.json());//parse incoming json to an object 
+app.use(express.json());//parse incoming req as a json and store it in req.body field
 app.use(userRouter);
 app.use(taskRouter);
 
@@ -49,14 +31,34 @@ const myFunction = async ()=>{
 myFunction() */
 
 
-const jwt = require('jsonwebtoken');
+/* const jwt = require('jsonwebtoken');
 
 const myFunction = async ()=>{
-    const token = jwt.sign({_id:'abc123'}, 'thisismynewcourse', {expiresIn:'7 seconds'});
+    const token = jwt.sign({_id:'abc123'}, 'thisismynewcourse', {expiresIn:'0 seconds'});
     console.log(token);
 
     const data = jwt.verify(token,'thisismynewcourse' );
     console.log(data);
 }
 
-myFunction();
+myFunction(); */
+
+
+//middlewares.
+/* app.use((req,res,next)=>{
+    //console.log(req.method,req.path);
+    //call next in chain.
+    if(req.method === 'GET'){
+        res.send('GET ')
+    }else {
+        next();//handle next route handler.//done with middleware.
+    }
+   
+}); */
+
+//Challenge
+/***middleware for maintenance mode. */
+/* app.use((req,res,next)=>{
+    res.status(503).send('<h2 style="color: red; display:flex; justify-content:center;">Site is under maintenance.</h2>')
+    next();
+}); */
