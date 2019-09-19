dabc5a3707e66a21b451bcb84b41a252cd13b794
rajan <rajangreedge@gmail.com>
2019-09-19 18:38:01 +0530

    testing uploading files

--- src/index.js
+++ src/index.js
@@ -7,14 +7,35 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-/* const multer = require('multer');
+const multer = require('multer');
+
 //config to upload
 upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits:{
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        //name of the file upload
+        //to find out the extension
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+          return cb(new Error('Please upload a word document(.doc or docx)'));
+        }
+
+        cb(undefined, true);
+
+        
+/*         //error
+        cb(new Error('File must be pdf'))
+        //successfully uploaded
+        cb(undefined, true);
+        //reject the upload
+        cb(undefined, false); */
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res)=>{
     res.send({"msg": "uploaded"});
-}) */
+})
 
 // router.
 app.use(express.json());//parse incoming req as a json and store it in req.body field
