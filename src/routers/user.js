e65b75dd9db3d9846298a4b9714bc332ccbe3c2a
rajan <rajangreedge@gmail.com>
2019-09-19 20:33:48 +0530

    new routes: GET user profile pic

--- src/routers/user.js
+++ src/routers/user.js
@@ -138,4 +138,23 @@ let upload = multer({
     
   })
 
+  /**
+   * @routes GET /users/:user_id/avatar
+   * @description 
+   * @access Private
+   */
+
+   router.get('/users/:id/avatar', async (req, res)=>{
+       try {
+           const user = await User.findById(req.params.id);
+           if(!user || !user.avatar){
+               throw new Error()
+           }
+           res.set('Content-Type','image/jpg');
+           res.send(user.avatar);
+       } catch (error) {
+           res.status(404).send();
+       }
+   })
+
 module.exports = router;
